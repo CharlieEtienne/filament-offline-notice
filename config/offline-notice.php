72201a3e738e1c56049e3123b62053f722ebe35e
CHARLIE ETIENNE <ce@web-nancy.fr>
2026-01-31 22:52:04 +0100

    configure package

--- config/offline-notice.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// config for CharlieEtienne/FilamentOfflineNotice
-return [
-
-];
--- src/FilamentOfflineNoticeServiceProvider.php
+++ src/FilamentOfflineNoticeServiceProvider.php
@@ -2,18 +2,12 @@
 
 namespace CharlieEtienne\FilamentOfflineNotice;
 
-use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
-use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
-use Filament\Support\Facades\FilamentIcon;
-use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
-use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use CharlieEtienne\FilamentOfflineNotice\Commands\FilamentOfflineNoticeCommand;
 use CharlieEtienne\FilamentOfflineNotice\Testing\TestsFilamentOfflineNotice;
 
 class FilamentOfflineNoticeServiceProvider extends PackageServiceProvider
@@ -24,38 +18,9 @@ class FilamentOfflineNoticeServiceProvider extends PackageServiceProvider
 
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package->name(static::$name)
-            ->hasCommands($this->getCommands())
-            ->hasInstallCommand(function (InstallCommand $command) {
-                $command
-                    ->publishConfigFile()
-                    ->publishMigrations()
-                    ->askToRunMigrations()
-                    ->askToStarRepoOnGitHub('charlieetienne/filament-offline-notice');
-            });
-
-        $configFileName = $package->shortName();
-
-        if (file_exists($package->basePath("/../config/{$configFileName}.php"))) {
-            $package->hasConfigFile();
-        }
-
-        if (file_exists($package->basePath('/../database/migrations'))) {
-            $package->hasMigrations($this->getMigrations());
-        }
-
-        if (file_exists($package->basePath('/../resources/lang'))) {
-            $package->hasTranslations();
-        }
-
-        if (file_exists($package->basePath('/../resources/views'))) {
-            $package->hasViews(static::$viewNamespace);
-        }
+            ->hasTranslations()
+            ->hasViews(static::$viewNamespace);
     }
 
     public function packageRegistered(): void {}
@@ -67,23 +32,6 @@ class FilamentOfflineNoticeServiceProvider extends PackageServiceProvider
             $this->getAssets(),
             $this->getAssetPackageName()
         );
-
-        FilamentAsset::registerScriptData(
-            $this->getScriptData(),
-            $this->getAssetPackageName()
-        );
-
-        // Icon Registration
-        FilamentIcon::register($this->getIcons());
-
-        // Handle Stubs
-        if (app()->runningInConsole()) {
-            foreach (app(Filesystem::class)->files(__DIR__ . '/../stubs/') as $file) {
-                $this->publishes([
-                    $file->getRealPath() => base_path("stubs/filament-offline-notice/{$file->getFilename()}"),
-                ], 'filament-offline-notice-stubs');
-            }
-        }
 
         // Testing
         Testable::mixin(new TestsFilamentOfflineNotice);
@@ -100,9 +48,7 @@ class FilamentOfflineNoticeServiceProvider extends PackageServiceProvider
     protected function getAssets(): array
     {
         return [
-            // AlpineComponent::make('filament-offline-notice', __DIR__ . '/../resources/dist/components/filament-offline-notice.js'),
-            // Css::make('filament-offline-notice-styles', __DIR__ . '/../resources/dist/filament-offline-notice.css'),
-            // Js::make('filament-offline-notice-scripts', __DIR__ . '/../resources/dist/filament-offline-notice.js'),
+            Css::make('filament-offline-notice-styles', __DIR__ . '/../resources/dist/filament-offline-notice.css'),
         ];
     }
 
@@ -111,9 +57,7 @@ class FilamentOfflineNoticeServiceProvider extends PackageServiceProvider
      */
     protected function getCommands(): array
     {
-        return [
-            FilamentOfflineNoticeCommand::class,
-        ];
+        return [];
     }
 
     /**
@@ -145,8 +89,6 @@ class FilamentOfflineNoticeServiceProvider extends PackageServiceProvider
      */
     protected function getMigrations(): array
     {
-        return [
-            'create_filament-offline-notice_table',
-        ];
+        return [];
     }
 }
